89aaadbc031c2aa5618a8eaa0eb69351f11b3fd2
Jordan Roessle <screamaimfire@me.com>
2021-05-21 16:50:58 -0700

    all routes seem functional, testing and proof reading required

--- app.py
+++ app.py
@@ -30,14 +30,27 @@ def home():
 		f"/api/v1.0/precipitation<br>"
 		f"/api/v1.0/stations<br>"
 		f"/api/v1.0/tobs<br>"
-		f"/api/v1.0/<start><br>"
-		f"/api/v1.0/<start>/<end><br>"
+		f"/api/v1.0/start_date<br>"
+		f"/api/v1.0/start_date/end_date<br>"
 		)
 
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
- 	return("precipitation")
+	# Open Session
+	session = Session(engine)
+
+	# Query date and prcp
+	query = session.query(Measurement.date, Measurement.prcp).all()
+	
+	# Close Session
+	session.close()
+
+	return_dict = {}
+	for result in query:
+		return_dict[result[0]] = result[1]
+
+	return(jsonify(return_dict))
 
 
 @app.route("/api/v1.0/stations")
@@ -61,7 +74,6 @@ def tobs():
 	# Date set-up
 	recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
 
-
 	# Change to datetime object
 	split = recent_date[0].split('-')
 	recent_date = dt.date(int(split[0]), int(split[1]), int(split[2]))
@@ -73,7 +85,7 @@ def tobs():
 	most_active = session.query(Measurement.station).group_by(Measurement.station).order_by(func.count(Measurement.station).desc()).first()[0]
 
 	# Find most active station's temperature in the last year
-	temperature_year = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date > year_ago).filter(Measurement.station == most_active).all()
+	temperature_year = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= year_ago).filter(Measurement.station == most_active).all()
 
 	# Close session
 	session.close()
@@ -81,11 +93,45 @@ def tobs():
 
 @app.route("/api/v1.0/<start>")
 def start(start):
- 	return("start")
+	# Open Session
+	session = Session(engine)
+
+	split = start.split('-')
+	try: 
+		start_date = dt.date(int(split[0]), int(split[1]), int(split[2]))
+	except:
+		return("The date you entered is invalid. Please try again while following this format: YYYY-MM-DD")
+
+	temperatures = session.query(Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).group_by(Measurement.date).filter(Measurement.date >= start_date).all()
+	
+	# Close Session
+	session.close()
+
+	return(jsonify(temperatures))
 
 @app.route("/api/v1.0/<start>/<end>")
 def startend(start, end):
- 	return("start end")
+	# Open Session
+	session = Session(engine)
+
+	split = start.split('-')
+	try: 
+		start_date = dt.date(int(split[0]), int(split[1]), int(split[2]))
+	except:
+		return("The FIRST date you entered is invalid. Please try again while following this format: YYYY-MM-DD")
+
+	split = end.split('-')
+	try: 
+		end_date = dt.date(int(split[0]), int(split[1]), int(split[2]))
+	except:
+		return("The SECOND date you entered is invalid. Please try again while following this format: YYYY-MM-DD")
+
+	temperatures = session.query(Measurement.date, func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).group_by(Measurement.date).filter(Measurement.date >= start_date).filter(Measurement.date <= end_date).all()
+	
+	# Close Session
+	session.close()
+	
+	return(jsonify(temperatures))
 
 if __name__ == '__main__':
 	app.run(debug=True)
